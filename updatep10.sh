a1d1ff881bfa2400693e6bc7cdfc83386974a7b8
lewa240 <lewaharukeiy240@gmail.com>
2026-05-18 20:15:38 +0900

    Update updatep10.sh

--- updatep10.sh
+++ updatep10.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-set -e  # Остановить выполнение при ошибке
+set -e
+
 echo "Инициализация..."
-# Удалить старый файл sisa.list, если он есть
+
 rm -f /etc/apt/sources.list.d/sisa.list
-# Закомментировать все строки во всех .list-файлах
+
 for file in /etc/apt/sources.list.d/*.list; do
     if [ -f "$file" ]; then
         sed -i 's/^[^#]/#&/' "$file"
     fi
 done
-# Создать новый файл sisa.list с нужными репозиториями
+
 cat << 'EOF' > /etc/apt/sources.list.d/sisa.list
 rpm http://10.0.50.50/sisa p10/x86_64 classic
 rpm http://10.0.50.50/sisa p10/noarch classic
 EOF
-# Обновить список пакетов
+
 apt-get update
+
 echo "Готово!"
