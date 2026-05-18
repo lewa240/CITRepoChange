438bf878cf1a13b5d7d13cefbb8313909cd02dcf
lewa240 <lewaharukeiy240@gmail.com>
2026-05-18 20:12:07 +0900

    Update updatep10.sh

--- updatep10.sh
+++ updatep10.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-
 echo "Инициализация..."
-
 rm -rf /etc/apt/sources.list.d/sisa.list
-
 for file in "/etc/apt/sources.list.d/"*.list; do
 	if [ -f "$file" ]; then
 		sed -i 's/^/#/' "$file"
 	fi
 done
-
 cat << EOF > "/etc/apt/sources.list.d/sisa.list"
 rpm http://10.0.50.50/sisa p10/x86_64 classic
 rpm http://10.0.50.50/sisa p10/noarch classic
 EOF
-
 apt-get update
-
 echo "Готово!"
